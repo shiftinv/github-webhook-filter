8622928bf9be003c61a1657eac96cd4cb3579cc2
shiftinv <me@shiftinv.cc>
2024-12-28 17:19:21 +0100

    feat: add scripts/sign.ts

---
+++ scripts/sign.ts
@@ -0,0 +1,10 @@
+import { sign } from "../src/crypto.ts";
+
+if (import.meta.main) {
+    const arg = Deno.args[0] ?? "";
+    if (arg.split("/").length !== 2) {
+        throw new Error(`usage: deno run --allow-env --env-file scripts/sign.ts <id>/<sig>`);
+    }
+
+    console.log(await sign(arg));
+}
